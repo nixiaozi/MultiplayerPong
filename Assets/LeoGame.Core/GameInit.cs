2eb26dcdf8283235935a9e63828f5cac6e508703
lee cheng <nixiaozi01@hotmail.com>
2020-09-23 08:52:51 +0800

    添加动态定义服务端IP地址的功能

--- Assets/LeoGame.Core/GameInit.cs
+++ Assets/LeoGame.Core/GameInit.cs
@@ -1,6 +1,12 @@
-﻿using Unity.Entities;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Unity.Entities;
 using Unity.NetCode;
 using Unity.Networking.Transport;
+using UnityEngine;
+using UnityEngine.Networking;
 
 /// <summary>
 /// 游戏准备阶段，分别初始化服务端和客户端的端口监听和套接层
@@ -13,12 +19,88 @@ public class GameInit : SystemBase
     {
     }
 
+    struct InitServerListen : IComponentData { }
+
     protected override void OnCreate()
     {
         RequireSingletonForUpdate<InitGameComponent>(); // 定义了 Update 需要有 InitGameComponent 这个组件才能运行
         // Create singleton, require singleton for update so system runs once
         EntityManager.CreateEntity(typeof(InitGameComponent));
+        //EntityManager.CreateEntity(typeof(InitServerListen));
     }
+
+
+    private string GetServerIp()
+    {
+        var url = "";
+        // 定义获取服务端IP地址的链接。
+        #region UNITY_CLIENT
+        url = "https://192.168.91.223/Default/GetGameServerIp";
+        #endregion
+
+        #region UNITY_EDITOR
+        url = "https://localhost:44394/Default/GetGameServerIp";
+        #endregion
+
+
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        UnityWebRequest www = UnityWebRequest.Post(url, formData);
+        www.SendWebRequest();
+        
+        
+        if (www.isNetworkError || www.isHttpError)
+        {
+            return null;
+        }
+        else
+        {
+            while (!www.isDone)
+            {
+                Thread.Sleep(200);
+            }
+            //Debug.Log("Form upload complete!");
+            var result = JsonUtility.FromJson<HttpResult<string>>(www.downloadHandler.text);
+            if (result != null && result.success)
+            {
+                return result.data;
+                /*var ip = result.data;
+                NetworkEndPoint ep = new NetworkEndPoint();
+                NetworkEndPoint.TryParse(ip, 7979, out ep);
+                network.Connect(ep); // 网络连接*/
+            }
+            else
+            {
+                return null;
+                //if (GetEntityQuery(new ComponentType[] { typeof(InitGameComponent) }).CalculateChunkCount() == 0)
+                //    EntityManager.CreateEntity(typeof(InitGameComponent));
+            }
+        }
+
+        
+        /*if (www.isNetworkError || www.isHttpError)
+        {
+            if (GetEntityQuery(new ComponentType[] { typeof(InitGameComponent) }).CalculateChunkCount() == 0)
+                EntityManager.CreateEntity(typeof(InitGameComponent));
+        }
+        else
+        {
+            //Debug.Log("Form upload complete!");
+            var result = JsonUtility.FromJson<HttpResult<string>>(www.downloadHandler.text);
+            if (result != null && result.success)
+            {
+                var ip = result.data;
+                NetworkEndPoint ep = new NetworkEndPoint();
+                NetworkEndPoint.TryParse(ip, 7979, out ep);
+                network.Connect(ep); // 网络连接
+            }
+            else
+            {
+                if (GetEntityQuery(new ComponentType[] { typeof(InitGameComponent) }).CalculateChunkCount() == 0)
+                    EntityManager.CreateEntity(typeof(InitGameComponent));
+            }
+        }*/ 
+    }
+
 
     protected override void OnUpdate()
     {
@@ -31,10 +113,20 @@ public class GameInit : SystemBase
             {
                 //to chose an IP instead of just always connecting to localhost uncomment the next line and delete the other two lines of code
                 //NetworkEndPoint.TryParse("IP address here", 7979, out NetworkEndPoint ep);
-                NetworkEndPoint ep = NetworkEndPoint.LoopbackIpv4;
-                ep.Port = 7979;
+                //NetworkEndPoint ep = NetworkEndPoint.LoopbackIpv4;
+                //ep.Port = 7979;
 
-                network.Connect(ep);
+                var ip = GetServerIp();
+                if (String.IsNullOrEmpty(ip))
+                {
+                    Debug.Log("获取服务端IP地址失败！");
+                }
+
+                NetworkEndPoint ep = new NetworkEndPoint();
+                NetworkEndPoint.TryParse(ip, 7979, out ep);
+                network.Connect(ep); // 网络连接
+
+                // network.Connect(ep);
             }
             else if (world.GetExistingSystem<ServerSimulationSystemGroup>() != null)
             {
@@ -43,10 +135,46 @@ public class GameInit : SystemBase
                 ep.Port = 7979;
                 network.Listen(ep);
 
-                
+                // network.Listen(ep);
+                //if(!network.Driver.IsCreated)
+                //    network.Listen(ep);
+
+                //if(network.Driver.Bind(ep)!=0)
+                //    network.Listen(ep);
+
+                //if (GetEntityQuery(new ComponentType[] { typeof(InitServerListen) }).CalculateChunkCount() == 0)
+                //{
+                //    NetworkEndPoint ep = NetworkEndPoint.AnyIpv4;
+                //    ep.Port = 7979;
+                //    network.Listen(ep);
+                //    EntityManager.DestroyEntity(GetSingletonEntity<InitServerListen>());
+                //}
+
             }
         }
     }
 
 
+}
+
+[Serializable]
+public class HttpResult<T>
+{
+    public string controlFuncDefine;
+
+    public Guid opreator;
+
+    public bool success;
+
+    public int dataCount;
+
+    public T data;
+
+    public int pageSize;
+
+    public int pageIndex;
+
+    public string displayMessage;
+
+    public string redirectUrl;
 }
